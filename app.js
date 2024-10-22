da7b1ab449aabfc2e6bff04542081bedaa5d1700
Ankur77720 <prajapatiankur8349@gmail.com>
2024-10-22 13:43:39 +0530

    middlewares

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const morgan = require('morgan')
 const app = express()
 
+app.use(morgan('dev'))
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+
+app.set("view engine", 'ejs')
+
+
+app.get('/',
+
+    (req, res, next) => {
+        const a = 5;
+        const b = 10;
+        console.log(a + b)
+
+        next()
+
+    }
+    , (req, res) => {
+        res.render('index')
+    })
 
 app.get('/about', (req, res) => {
     res.send('About Page')
