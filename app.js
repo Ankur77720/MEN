74795e9eedd781b17e0b81ededafb3f5e39680cd
Ankur77720 <prajapatiankur8349@gmail.com>
2024-10-22 18:28:07 +0530

    form handling done

--- app.js
+++ app.js
@@ -4,23 +4,16 @@ const app = express()
 
 app.use(morgan('dev'))
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 
 app.set("view engine", 'ejs')
 
 
-app.get('/',
-
-    (req, res, next) => {
-        const a = 5;
-        const b = 10;
-        console.log(a + b)
-
-        next()
-
-    }
-    , (req, res) => {
-        res.render('index')
-    })
+app.get('/', (req, res) => {
+    res.render('index')
+})
 
 app.get('/about', (req, res) => {
     res.send('About Page')
@@ -28,6 +21,11 @@ app.get('/about', (req, res) => {
 
 app.get('/profile', (req, res) => {
     res.send('Profile Page')
+})
+
+app.post('/get-form-data', (req, res) => {
+    console.log(req.body)
+    res.send('data received')
 })
 
 app.listen(3000)
